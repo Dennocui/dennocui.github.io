db339a3c8c6ef2e2429fd558d5e63983cf8e7b7d
dennocui <dennochui@gmail.com>
2019-11-15 00:45:02 +0300

    1

--- snc/models.py
+++ snc/models.py
@@ -54,7 +54,7 @@ class TechnicalSkill(models.Model):
     one_on_one = models.CharField('One on One', max_length=15, choices=SKILL_LEVEL, default=WORKON,)
     support = models.CharField('Support Play',max_length=15, choices=SKILL_LEVEL, default=WORKON,)
     high_ball = models.CharField('High Ball', max_length=15, choices=SKILL_LEVEL, default=WORKON,)
-    kicking = models.CharField('Kicking(Grubber/Punt/Chip)', max_length=15, choices=SKILL_LEVEL, blank=True,)
+    kicking = models.CharField('Kicking(Grubber/Punt/Chip)', max_length=15, choices=SKILL_LEVEL, null=True,)
     commment = models.TextField()
 
     created_at = models.DateTimeField('Date Created', auto_now=True, auto_now_add=False)
